ecfef35341e6927251d907156e5f9e07f1f40e99
kk1341 <kazz9276@gmail.com>
2024-08-26 19:38:30 +0900

    アプリケーションコンテキストの追加

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, current_app, g
 
 app = Flask(__name__)
 
@@ -22,3 +22,25 @@ def hello():
 def show_name(name):
     # 変数をテンプレートエンジンに渡す
     return render_template("index.html", name=name)
+
+
+with app.test_request_context():
+    # /
+    print(url_for("index"))
+    # /hello/world
+    print(url_for("hello-endpoint", name="world"))
+    # /name/fukuhara?page=1
+    print(url_for("show_name", name="fukuhara", page="1"))
+
+
+# アプリケーションコンテキストを取得してスタックへpushする
+ctx = app.app_context()
+ctx.push()
+
+# current_appにアクセスが可能になる
+print(current_app)
+# >> apps.minimalapp.app
+
+# グローバルなテンポラリ領域に値を設定する
+g.connection = "connection"
+print(g.connection)
