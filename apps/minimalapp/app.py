7c86c75be662fee7fc34102ba4ed0fbac900cf2d
kk1341 <kazz9276@gmail.com>
2024-08-26 12:55:27 +0900

    テンプレートの作成

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,14 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 # URLと実行する関数をマッピングする
 @app.route("/")
 def index():
     return "Hello, Flaskbook!"
 
-@app.route("/hello/<name>",
-           methods=["GET", "POST"],
-           endpoint="hello-endpoint")
-def hello(name):
-    return f"Hello, {name}!"
+
+# show_nameエンドポイントを作る
+@app.route("/name/<name>")
+def show_name(name):
+    # 変数をテンプレートエンジンに渡す
+    return render_template("index.html", name=name)
