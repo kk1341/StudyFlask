1a8792efc563b13cbc9d58834d5e3e095f69cd34
kk1341 <kazz9276@gmail.com>
2024-08-28 00:29:34 +0900

    問い合わせフォームを返すエンドポイントを作成

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, current_app, g
+from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
@@ -24,23 +24,17 @@ def show_name(name):
     return render_template("index.html", name=name)
 
 
-with app.test_request_context():
-    # /
-    print(url_for("index"))
-    # /hello/world
-    print(url_for("hello-endpoint", name="world"))
-    # /name/fukuhara?page=1
-    print(url_for("show_name", name="fukuhara", page="1"))
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
-# アプリケーションコンテキストを取得してスタックへpushする
-ctx = app.app_context()
-ctx.push()
+@app.route("/contact/complete", methods=['GET', "POST"])
+def contact_complete():
+    if request.method == "POST":
+        # メールを送る
 
-# current_appにアクセスが可能になる
-print(current_app)
-# >> apps.minimalapp.app
+        # contactエンドポイントへリダイレクトする
+        return redirect(url_for("contact_complete"))
 
-# グローバルなテンポラリ領域に値を設定する
-g.connection = "connection"
-print(g.connection)
+    return render_template("contact_complete.html")
