ecaae28214823bc44910a6e65babcd67dd91f196
kk1341 <kazz9276@gmail.com>
2024-09-01 17:26:10 +0900

    flashメッセージの表示とバリデーションチェックの追加

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import (
+    Flask,
+    render_template,
+    url_for,
+    request,
+    redirect,
+    flash,
+)
 
 app = Flask(__name__)
+# SECRET_KEYを追加
+app.config["SELECT_KEY"] = ""
 
 
 # URLと実行する関数をマッピングする
@@ -10,9 +19,11 @@ def index():
 
 
 # Endpoint : URIと紐づけられた関数名または関数につけた名前
-@app.route("/hello/<name>",
-           methods=['GET', 'POST'],
-           endpoint="hello-endpoint",)
+@app.route(
+    "/hello/<name>",
+    methods=["GET", "POST"],
+    endpoint="hello-endpoint",
+)
 def hello():
     return "Hello, World!"
 
@@ -29,9 +40,39 @@ def contact():
     return render_template("contact.html")
 
 
-@app.route("/contact/complete", methods=['GET', "POST"])
+@app.route("/contact/complete", methods=["GET", "POST"])
 def contact_complete():
     if request.method == "POST":
+        # メールを送る
+        # form属性を使ってPOSTされたフォームの値を取得する
+        username = request.form["username"]
+        email = request.form["email"]
+        description = request.form["description"]
+
+        # 入力チェック
+        is_valid = True
+
+        if not username:
+            flash("ユーザー名は必須です")
+            is_valid = False
+
+        if not email:
+            flash("メールアドレスは必須です")
+            is_valid = False
+
+        try:
+            validate_email(email)
+        except EmailNotValidError:
+            flash("メールアドレスの形式で入力してください")
+            is_valid = False
+
+        if not description:
+            flash("お問い合わせ内容は必必須です")
+            is_valid = False
+
+        if not is_valid:
+            return redirect(url_for("contact"))
+
         # メールを送る
 
         # contactエンドポイントへリダイレクトする
